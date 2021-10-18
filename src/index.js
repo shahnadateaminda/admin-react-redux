f2877720106f81ff1bb5efee41ef0ac5b9c805a4
shahnad@ateamindia.com <shahnad@ateamindia.com>
2021-10-18 17:37:04 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -10,10 +10,8 @@ import store from './redux/store'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-  
       <App />
- 
-      </Provider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
